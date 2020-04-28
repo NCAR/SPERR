33623d49c4e9f2f882dcdf974cca2b392556dde3
Samuel Li <shaomeng@ucar.edu>
2020-04-27 18:01:39 -0600

    add a print statement

--- utilities/decompressor_3d.cpp
+++ utilities/decompressor_3d.cpp
@@ -71,5 +71,9 @@ int main( int argc, char* argv[] )
         return 1;
     }
 
+    // Print some basic info:
+    std::printf(" Decompress volume with dimensions (%u, %u, %u). BPP = %f\n", 
+                  dim_x, dim_y, dim_z, float(bit_budget)/float(total_vals)  );
+
     return 0;
 }
