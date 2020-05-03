bded488cc20112451e27a80e74caef96ba0f9d47
Samuel Li <shaomeng@ucar.edu>
2020-05-02 20:46:40 -0600

    add performance evaluation of the new LSP representation

--- evaluations/evaluate_compress.sh
+++ evaluations/evaluate_compress.sh
@@ -2,26 +2,26 @@
 
 bpp=(0.25 0.5 1 2 4)
 
-for t in ${bpp[*]}; do
-    ../build/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ tmp $t >> 128_cube.result
-done
-
-for t in ${bpp[*]}; do
-    ../build/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ tmp $t >> 256_cube.result
-done
-
-for t in ${bpp[*]}; do
-    ../build/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ tmp $t >> 512_cube.result
-done
-
 # for t in ${bpp[*]}; do
-#     ../build/decompressor_3d ../build/128_bpp_5.bitstream $t tmp >> 128_cube.result
+#     ../build-gcc-9.3/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ tmp $t >> 128_cube.result
 # done
 # 
 # for t in ${bpp[*]}; do
-#     ../build/decompressor_3d ../build/256_bpp_5.bitstream $t tmp >> 256_cube.result
+#     ../build-gcc-9.3/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ tmp $t >> 256_cube.result
 # done
 # 
 # for t in ${bpp[*]}; do
-#     ../build/decompressor_3d ../build/512_bpp_5.bitstream $t tmp >> 512_cube.result
+#     ../build-gcc-9.3/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ tmp $t >> 512_cube.result
 # done
+
+for t in ${bpp[*]}; do
+    ../build-gcc-9.3/decompressor_3d ./128.bitstream $t tmp >> 128_cube.result
+done
+
+for t in ${bpp[*]}; do
+    ../build-gcc-9.3/decompressor_3d ./256.bitstream $t tmp >> 256_cube.result
+done
+
+for t in ${bpp[*]}; do
+    ../build-gcc-9.3/decompressor_3d ./512.bitstream $t tmp >> 512_cube.result
+done
