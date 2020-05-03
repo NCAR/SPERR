a89443b4da250c21bbf6e97ba143be95e237461a
Samuel Li <shaomeng@ucar.edu>
2020-05-02 21:41:41 -0600

    separate results from gcc-5.4 vs. 9.3

--- evaluations/evaluate_compress.sh
+++ evaluations/evaluate_compress.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
 bpp=(0.25 0.5 1 2 4)
-
-# for t in ${bpp[*]}; do
-#     ../build-gcc-9.3/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ tmp $t >> 128_cube.result
-# done
-# 
-# for t in ${bpp[*]}; do
-#     ../build-gcc-9.3/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ tmp $t >> 256_cube.result
-# done
-# 
-# for t in ${bpp[*]}; do
-#     ../build-gcc-9.3/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ tmp $t >> 512_cube.result
-# done
+build_dir_name=build
 
 for t in ${bpp[*]}; do
-    ../build-gcc-9.3/decompressor_3d ./128.bitstream $t tmp >> 128_cube.result
+    ../$build_dir_name/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ tmp $t >> 128_cube.result
 done
 
 for t in ${bpp[*]}; do
-    ../build-gcc-9.3/decompressor_3d ./256.bitstream $t tmp >> 256_cube.result
+    ../$build_dir_name/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ tmp $t >> 256_cube.result
 done
 
 for t in ${bpp[*]}; do
-    ../build-gcc-9.3/decompressor_3d ./512.bitstream $t tmp >> 512_cube.result
+    ../$build_dir_name/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ tmp $t >> 512_cube.result
 done
+
+
+../$build_dir_name/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ 128.bitstream 4.1
+../$build_dir_name/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ 267.bitstream 4.1
+../$build_dir_name/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ 512.bitstream 4.1
+
+
+for t in ${bpp[*]}; do
+    ../$build_dir_name/decompressor_3d ./128.bitstream $t tmp >> 128_cube.result
+done
+
+for t in ${bpp[*]}; do
+    ../$build_dir_name/decompressor_3d ./256.bitstream $t tmp >> 256_cube.result
+done
+
+for t in ${bpp[*]}; do
+    ../$build_dir_name/decompressor_3d ./512.bitstream $t tmp >> 512_cube.result
+done
+
+
+rm -f 128.bitstream 256.bitstream 512.bitstream tmp
