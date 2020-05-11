3d5e9210b5b146f37981b54a0a18ffdc9c96e56d
Samuel Li <shaomeng@ucar.edu>
2020-05-11 15:02:12 -0600

    add May_11 evaluation results

--- evaluations/evaluate_compress.sh
+++ evaluations/evaluate_compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-bpp=(0.25 0.5 1 2 4)
-build_dir_name=build
+bpp=(0.125 0.25 0.5 1 2 4)
+build_dir_name=build-gcc-9.3
 
 for t in ${bpp[*]}; do
     ../$build_dir_name/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ tmp $t >> 128_cube.result
@@ -17,7 +17,7 @@ done
 
 
 ../$build_dir_name/compressor_3d ../test_data/wmag128.float 128 128 128 XYZ 128.bitstream 4.1
-../$build_dir_name/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ 267.bitstream 4.1
+../$build_dir_name/compressor_3d ../test_data/wmag256.float 256 256 256 XYZ 256.bitstream 4.1
 ../$build_dir_name/compressor_3d ../test_data/wmag512.float 512 512 512 XYZ 512.bitstream 4.1
 
 
