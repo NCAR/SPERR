003e6270b4dd03fa577eaa1fad86c74f2c134322
Samuel Li <shaomenglee@gmail.com>
2020-06-18 18:53:33 -0600

    WIP

--- src/SPECK3D_Err.cpp
+++ src/SPECK3D_Err.cpp
@@ -1,5 +1,10 @@
 #include "SPECK3D_Err.h"
 
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <cstring>
+
 void speck::SPECK3D_Err::reserve(size_t num)
 {
     m_LOS.reserve(num);
@@ -30,3 +35,149 @@ void speck::SPECK3D_Err::set_tolerance(float t)
 {
     m_tolerance = t;
 }
+
+void speck::SPECK3D_Err::m_initialize_LIS()
+{
+    std::array<size_t, 3> num_of_parts; // how many times each dimension could be partitioned?
+    num_of_parts[0] = speck::num_of_partitions( m_dim_x );
+    num_of_parts[1] = speck::num_of_partitions( m_dim_y );
+    num_of_parts[2] = speck::num_of_partitions( m_dim_z );
+    size_t num_of_sizes = 1;
+    for (size_t i = 0; i < 3; i++)
+        num_of_sizes += num_of_parts[i];
+
+    // initialize LIS
+    m_LIS.clear();
+    m_LIS.resize(num_of_sizes);
+    m_LIS_garbage_cnt.assign(num_of_sizes, 0);
+
+    // Starting from a set representing the whole volume, identify the smaller sets
+    //   and put them in LIS accordingly.
+    SPECKSet3D big;
+    big.length_x = uint32_t(m_dim_x); // Truncate 64-bit int to 32-bit, but should be OK.
+    big.length_y = uint32_t(m_dim_y); // Truncate 64-bit int to 32-bit, but should be OK.
+    big.length_z = uint32_t(m_dim_z); // Truncate 64-bit int to 32-bit, but should be OK.
+
+    // clang-format off
+    const auto num_of_xforms_xy = speck::num_of_xforms(std::min(m_dim_x, m_dim_y));
+    const auto num_of_xforms_z  = speck::num_of_xforms(m_dim_z);
+    size_t     xf               = 0;
+    // clang-format on
+    std::array<SPECKSet3D, 8> subsets;
+    while (xf < num_of_xforms_xy && xf < num_of_xforms_z) {
+        speck::partition_S_XYZ(big, subsets);
+        big = subsets[0];
+        for (size_t i = 1; i < 8; i++) {
+            const auto parts = subsets[i].part_level;
+            m_LIS[parts].emplace_back(subsets[i]);
+        }
+        xf++;
+    }
+
+    // One of these two conditions could happen if num_of_xforms_xy != num_of_xforms_z
+    if (xf < num_of_xforms_xy) {
+        std::array<SPECKSet3D, 4> sub4;
+        while (xf < num_of_xforms_xy) {
+            speck::partition_S_XY(big, sub4);
+            big = sub4[0];
+            for (size_t i = 1; i < 4; i++) {
+                const auto parts = sub4[i].part_level;
+                m_LIS[parts].emplace_back(sub4[i]);
+            }
+            xf++;
+        }
+    } else if (xf < num_of_xforms_z) {
+        std::array<SPECKSet3D, 2> sub2;
+        while (xf < num_of_xforms_z) {
+            speck::partition_S_Z(big, sub2);
+            big              = sub2[0];
+            const auto parts = sub2[1].part_level;
+            m_LIS[parts].emplace_back(sub2[1]);
+            xf++;
+        }
+    }
+
+    // Right now big is the set that's most likely to be significant, so insert
+    // it at the front of it's corresponding vector. One-time expense.
+    const auto parts = big.part_level;
+#ifdef NO_CPP14
+    m_LIS[parts].insert(m_LIS[parts].begin(), big);
+#else
+    m_LIS[parts].insert(m_LIS[parts].cbegin(), big);
+#endif
+}
+
+void speck::SPECK3D_Err::m_clean_LIS()
+{
+    std::vector<SPECKSet3D> tmpv;
+
+    for (size_t tmpi = 1; tmpi <= m_LIS_garbage_cnt.size(); tmpi++) {
+
+        // Because lists towards the end tend to have bigger sizes, we look at
+        // them first. This practices should reduce the number of memory allocations.
+        const auto i = m_LIS_garbage_cnt.size() - tmpi;
+
+        // Only consolidate memory if the garbage count is more than half
+        if (m_LIS_garbage_cnt[i] > m_LIS[i].size() / 2) {
+            auto& list = m_LIS[i];
+            tmpv.clear();
+            tmpv.reserve(list.size()); // will leave half capacity unfilled, so the list
+                                       // won't need a memory re-allocation for a while.
+            std::copy_if(list.cbegin(), list.cend(), std::back_inserter(tmpv),
+                         [](const SPECKSet3D& s) { return s.type != SetType::Garbage; });
+            std::swap(list, tmpv);
+            m_LIS_garbage_cnt[i] = 0;
+        }
+    }
+}
+
+auto speck::SPECK3D_Err::m_ready_to_encode() const -> bool
+{
+    if (m_dim_x == 0 || m_dim_y == 0 || m_dim_z == 0)
+        return false;
+    if( m_tolerance <= 0.0f )
+        return false;
+    if( m_LOS.empty() )
+        return false;
+
+    return true;
+
+}
+
+auto speck::SPECK3D_Err::encode() -> int
+{
+    int rv;
+    if( (rv = m_ready_to_encode()) != 0 )
+        return rv;
+    m_encode_mode = true;
+
+    // initialize other data structures
+    m_initialize_LIS();
+    m_outlier_cnt = m_LOS.size();   // Initially everyone in LOS is an outlier
+    m_q.reserve( m_outlier_cnt );
+    for( const auto& o : m_LOS )
+        m_q.push_back( std::abs( o.err ) );
+    m_err_hat.assign( m_outlier_cnt, 0.0f );
+    m_LSP.reserve( m_outlier_cnt ); // Every outlier will end up in m_LSP
+    m_LSP_newly.reserve( m_outlier_cnt );
+
+    // Find the maximum q, and decide m_max_coeff_bits
+    auto max_q       = *std::max_element( m_q.cbegin(), m_q.cend() );
+    auto max_bits_f  = std::floor( std::log2(max_q) );
+    m_max_coeff_bits = int32_t( max_bits_f );
+    m_threshold      = std::pow(2.0f, max_bits_f);
+
+    // Start the iterations! 
+    for( size_t bitplane = 0; bitplane < 128; bitplane++ ) {
+
+
+    }
+    
+    return 0;    
+}
+
+
+
+
+
+
--- src/SPECK3D_Err.h
+++ src/SPECK3D_Err.h
@@ -14,6 +14,7 @@ public:
     //
     // Trival input/output functions
     //
+    // Input
     // Reserve space for a certain number of outliers. This is optional though.
     void reserve(size_t);                       
     void add_outlier(uint32_t, uint32_t, uint32_t, float); // add a single outlier.
@@ -21,24 +22,44 @@ public:
     void set_dims(size_t, size_t, size_t);                 // set the volume dimensions
     void set_tolerance(float);                             // set error tolerance
 
+    // Output
+    //std::vector<Outlier> release_outliers();        // Release ownership of decoded outliers
+    //auto get_num_of_outliers() const -> size_t;     // How many outliers are decided?
+    //auto get_ith_outlier(size_t) const -> Outlier;  // Return a single outlier
+
+    // Action methods
+    auto encode() -> int;
+
 private:
+    //
+    // Private methods
+    //
+    void m_initialize_LIS();
+    void m_clean_LIS();
+    auto m_ready_to_encode() const -> bool;
+
+
     //
     // Private data members
     //
-    double  m_threshold      = 0.0; // Threshold that's used for quantization
-    size_t  m_bit_idx        = 0;   // Used for decode. Which bit we're at?
-    size_t  m_dim_x          = 0;   // 3D volume dims
+    // Properties that do not change
+    size_t  m_dim_x          = 0;    // 3D volume dims
     size_t  m_dim_y          = 0;
     size_t  m_dim_z          = 0;
+    float   m_tolerance      = 0.01; // Gamma in the algorithm description
     bool    m_encode_mode    = true; // Encode (true) or Decode (false) mode?
     int32_t m_max_coeff_bits = 0;    // = log2(max_coefficient)
-    float   m_tolerance      = 0.01; // Gamma in the algorithm description
-    size_t  m_outlier_cnt    = 0;    // How many data points are still exceeding the tolerance?
 
+    // Global variables that change and facilitate the process.
+    size_t  m_outlier_cnt    = 0;    // How many data points are still exceeding the tolerance?
+    float   m_threshold      = 0.0;  // Threshold that's used for quantization
+    size_t  m_bit_idx        = 0;    // Used for decode. Which bit we're at?
+
+    std::vector<bool>    m_bit_buffer;
     std::vector<Outlier> m_LOS;      // List of OutlierS. This list is not altered by encoding.
     std::vector<float>   m_q;        // Q list in the algorithm description. This list is
                                      // constantly refined by the refinement pass.
-    std::vector<float> m_err_hat;    // err_hat list in the algorithm description. This list
+    std::vector<float>  m_err_hat;   // err_hat list in the algorithm description. This list
                                      // contains the values that would be reconstructed.
     std::vector<size_t> m_LSP;       // Records locations of significant pixels
     std::vector<bool>   m_LSP_newly; // Records if this pixel is newly significant or not.
