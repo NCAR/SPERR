4a9520a685baa7edb16764e96c0b257d038f2960
Samuel Li <shaomeng@cisl-vapor>
2021-02-10 23:05:41 -0700

    probe works much better

--- utilities/probe_3d_qz.cpp
+++ utilities/probe_3d_qz.cpp
@@ -20,74 +20,60 @@
 auto test_configuration( const float* in_buf, std::array<size_t, 3> dims, 
                          int32_t qz_level, float tolerance ) -> int 
 {
-    // Here we use individual CDF and SPECK encoders instead of 
-    // `SPECK3D_Compressor` and `SPECK3D_Decompressor.`
-    // That is because `SPECK3D_Compressor` does outlier correction already
-    // in QZ_TERM mode!
-    // A minor benefit is that we can reuse the same objects for encoding and decoding.
-
-    // Step 1: Wavelet transform
-    auto start_time = std::chrono::steady_clock::now();
+    // Setup
     const size_t total_vals = dims[0] * dims[1] * dims[2];
-    speck::CDF97 cdf;
-    cdf.set_dims( dims[0], dims[1], dims[2] );   
-    cdf.copy_data( in_buf, total_vals );
-    cdf.dwt3d();
-    auto cdf_out = cdf.release_data();
-    
-    // Step 2: SPECK3D encoding
-    speck::SPECK3D speck;
-    speck.set_dims( dims[0], dims[1], dims[2] );
-    speck.set_image_mean( cdf.get_mean() );
-    speck.set_quantization_term_level( qz_level );
-    speck.take_data( std::move(cdf_out.first), cdf_out.second );
-    auto rtn  = speck.encode();
+    SPECK3D_Compressor compressor( dims[0], dims[1], dims[2] );
+    compressor.copy_data( in_buf, total_vals );
+    compressor.set_qz_level( qz_level );
+    compressor.set_tolerance( tolerance );
+
+    // Perform compression
+    auto start_time = std::chrono::steady_clock::now();
+    auto rtn  = compressor.compress();
     if(  rtn != RTNType::Good )
         return 1;
     auto end_time = std::chrono::steady_clock::now();
     auto diff_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
-    std::cout << "    Compression takes time: " << diff_time << "ms\n";
-    const auto num_bits = speck.get_bit_buffer_size();
-    printf("    Total compressed size in bytes: ~%ld, average bpp: ~%.2f\n", 
-                num_bits / 8, float(num_bits) / float(total_vals) );
+    std::cout << " -> Compression takes time: " << diff_time << "ms\n";
+    auto encoded_stream = compressor.get_encoded_bitstream();
+    auto otl_count = compressor.get_outlier_count();
 
-    // Step 3: SPECK3D decoding
+    // Print some analysis
+    if( speck::empty_buf( encoded_stream ) )
+        return 1;
+    printf("    Total compressed size in bytes = %ld, average bpp = %.2f\n", 
+               encoded_stream.second, float(encoded_stream.second * 8) / float(total_vals) );
+    if( otl_count.first == 0 ) {
+        printf("    There are no outliers at this quantization level!\n");
+    }
+    else {
+        printf("    Outliers: num = %ld, pct = %.3f%%, bpp ~ %.2f, "
+                   "using total storage ~ %.2f%%\n",
+                otl_count.first, float(otl_count.first * 100) / float(total_vals),
+                float(otl_count.second * 8) / float(otl_count.first),
+                float(otl_count.second * 100) / float(encoded_stream.second) );
+    }
+
+    // Perform decompression
+    SPECK3D_Decompressor decompressor;
+    decompressor.take_bitstream( std::move(encoded_stream) );
     start_time = std::chrono::steady_clock::now();
-    rtn = speck.decode();
+    rtn = decompressor.decompress();
     if( rtn != RTNType::Good )
         return 1;
-    auto speck_out = speck.release_data();
-
-    // Step 4: Inverse wavelet transform
-    cdf.take_data( std::move(speck_out.first), speck_out.second );
-    cdf.idwt3d();
-    auto vol_d = cdf.get_read_only_data();
-    assert( vol_d.second == total_vals );
-    auto reconstruct = std::make_unique<float[]>( total_vals );
-    for( size_t i = 0; i < total_vals; i++ )
-        reconstruct[i] = vol_d.first[i];
     end_time = std::chrono::steady_clock::now();
     diff_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_time-start_time).count();
-    std::cout << "    Decompression takes time: " << diff_time << "ms\n";
+    std::cout << " -> Decompression takes time: " << diff_time << "ms\n";
+    auto decoded_volume = decompressor.get_decompressed_volume_f();
+    if( speck::empty_buf( decoded_volume ) || decoded_volume.second != total_vals )
+        return 1;
 
-    // Step 5: Calculate statistics
+    // Collect statistics
     float rmse, lmax, psnr, arr1min, arr1max;
-    speck::calc_stats( in_buf, reconstruct.get(), total_vals,
+    speck::calc_stats( in_buf, decoded_volume.first.get(), total_vals,
                        &rmse, &lmax, &psnr, &arr1min, &arr1max);
     printf("    Original data range = (%.2e, %.2e)\n", arr1min, arr1max);
-    printf("    Reconstructed data RMSE = %.2e, L-Infty = %.2e, PSNR = %.2f\n", rmse, lmax, psnr);
-
-    // Step 6: Count the number of outliers. 
-    //         Estimate new bpp by approximating each encoded outlier taking 32 bits.
-    auto num_outlier = std::inner_product( in_buf, in_buf + total_vals,
-                                           reconstruct.get(), size_t{0},
-                                           std::plus<>(),
-                [tolerance](auto a, auto b){return std::abs(a-b) > tolerance ? 1 : 0;});
-    printf("    With qz level = %d and tolerance = %.2e, "
-           "num of outliers = %ld, pct = %.3f%%\n",
-           qz_level, tolerance, num_outlier, float(num_outlier * 100) / float(total_vals) );
-    printf("    Encoding these outliers will increase bpp to: ~%.2f\n",
-                float(num_bits + num_outlier * 32) / float(total_vals) );
+    printf("    Reconstructed data RMSE = %.2e, L-Infty = %.2e, PSNR = %.2fdB\n", rmse, lmax, psnr);
 
     return 0;
 }
@@ -155,8 +141,7 @@ int main( int argc, char* argv[] )
     std::cin >> answer;
     while ( std::tolower(answer) == 'y' ) {
         int32_t tmp;
-        std::cout << "Please input a new qz level to test "
-                     "(larger values mean more compression):  ";
+        std::cout << "Please input a new qz level to test: ";
         std::cin >> tmp;
         while (tmp < qz_level - 10 || tmp > qz_level + 10) {
             printf("Please input a qz level within (-10, 10) of %d:  ", qz_level);
