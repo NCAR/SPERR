23899039a7fef74afa7409e06e3026b57c772844
Samuel Li <shaomeng@cisl-vapor>
2021-09-27 23:24:41 -0600

    clang-format

--- src/sperr_helper.cpp
+++ src/sperr_helper.cpp
@@ -12,10 +12,10 @@ auto sperr::num_of_xforms(size_t len) -> size_t
   assert(len > 0);
   // I decide 8 is the minimal length to do one level of xform.
   auto f = std::log2(double(len) / 8.0);
-  auto num = f < 0.0 ? size_t{0}: size_t(f) + 1;
+  auto num = f < 0.0 ? size_t{0} : size_t(f) + 1;
   // I also decide that no matter what the input size is,
   // six (6) is the maxinum number of transforms to do.
-  return std::min( num, size_t{6} );
+  return std::min(num, size_t{6});
 }
 
 auto sperr::num_of_partitions(size_t len) -> size_t
