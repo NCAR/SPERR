f2693f18f6555a4316fd91e2415774a7f0d9b9d6
Samuel Li <shaomeng@users.noreply.github.com>
2021-11-08 23:24:04 -0700

    implement show_version utility (#116)
    * implement show_version utility
    
    * add show_version.cpp
    
    * minor fix
    
    Co-authored-by: Samuel Li <shaomeng@cisl-vapor>

---
+++ utilities/show_version.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "SperrConfig.h"
+
+int main()
+{
+  std::cout << "SPERR version: " << SPERR_VERSION_MAJOR << "." << SPERR_VERSION_MINOR << std::endl;
+  std::cout << "Based on code SHA1: " << SPERR_GIT_SHA1 << std::endl;
+}
