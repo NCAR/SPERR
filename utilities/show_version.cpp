dd83f667c269afac4ec49da8b0d6b65d6ab23552
Samuel Li <shaomenglee@gmail.com>
2021-11-12 10:10:31 -0700

    add more version output

--- utilities/show_version.cpp
+++ utilities/show_version.cpp
@@ -4,5 +4,6 @@
 int main()
 {
   std::cout << "SPERR version: " << SPERR_VERSION_MAJOR << "." << SPERR_VERSION_MINOR << std::endl;
-  std::cout << "Based on code SHA1: " << SPERR_GIT_SHA1 << std::endl;
+  std::cout << "Based on code Branch: " << SPERR_GIT_BRANCH << std::endl;
+  std::cout << "Based on code SHA1  : " << SPERR_GIT_SHA1 << std::endl;
 }
