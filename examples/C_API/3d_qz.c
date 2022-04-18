da6b22d32e0b00277a45bda267bdea1a23bae540
Samuel Li <shaomeng@cisl-vapor>
2022-04-18 16:30:37 -0600

    simple C API for 3D cases also works!

--- examples/C_API/3d_qz.c
+++ examples/C_API/3d_qz.c
@@ -28,8 +28,8 @@ size_t read_file(const char* filename, void** dst)
 
 int main(int argc, char** argv)
 {
-  if (argc < 6) {
-    printf("Usage: ./a.out filename dimx, dimy, qz_lev, tolerance [-d]\n");
+  if (argc < 7) {
+    printf("Usage: ./a.out filename dimx, dimy, dimz, qz_lev, tolerance [-d]\n");
     printf("  Note: -d is optional to indicate that the input is in double format\n");
     return 1;
   }
@@ -37,10 +37,11 @@ int main(int argc, char** argv)
   const char* filename = argv[1];
   const size_t dimx = (size_t)atol(argv[2]);
   const size_t dimy = (size_t)atol(argv[3]);
-  const int32_t q = (int32_t)atoi(argv[4]);
-  const double tol = atof(argv[5]);
+  const size_t dimz = (size_t)atol(argv[4]);
+  const int32_t q = (int32_t)atoi(argv[5]);
+  const double tol = atof(argv[6]);
   int is_float = 1;
-  if (argc == 7)
+  if (argc == 8)
     is_float = 0;
 
   /* Read in a file and put its content in `inbuf` */
@@ -48,37 +49,38 @@ int main(int argc, char** argv)
   size_t inlen = read_file(filename, &inbuf);
   if (inlen == 0)
     return 1;
-  if (is_float && inlen != sizeof(float) * dimx * dimy)
+  if (is_float && inlen != sizeof(float) * dimx * dimy * dimz)
     return 1;
-  if (!is_float && inlen != sizeof(double) * dimx * dimy)
+  if (!is_float && inlen != sizeof(double) * dimx * dimy * dimz)
     return 1;
 
   /* Compress `inbuf` and put the compressed bitstream in `bitstream` */
   void* bitstream = NULL; /* Will be free'd later */
   size_t stream_len = 0;
-  int rtn = sperr_qzcomp_2d(inbuf, is_float, dimx, dimy, q, tol, &bitstream, &stream_len);
+  int rtn = sperr_qzcomp_3d(inbuf, is_float, dimx, dimy, dimz, q, tol, &bitstream, &stream_len);
   if (rtn != 0) {
     printf("Compression error with code %d\n", rtn);
     return 1;
   }
 
-  /* Decompress `bitstream` and put the decompressed slice in `outbuf` */
+  /* Decompress `bitstream` and put the decompressed volume in `outbuf` */
   void* outbuf = NULL; /* Will be free'd later */
   size_t out_dimx = 0;
   size_t out_dimy = 0;
-  rtn = sperr_qzdecomp_2d(bitstream, stream_len, is_float, &out_dimx, &out_dimy, &outbuf);
+  size_t out_dimz = 0;
+  rtn = sperr_qzdecomp_3d(bitstream, stream_len, is_float, &out_dimx, &out_dimy, &out_dimz, &outbuf);
   if (rtn != 0) {
     printf("Decompression error with code %d\n", rtn);
     return 1;
   }
 
   /* Write the decompressed buffer to disk */
-  FILE* f = fopen("./out.buf", "w");
+  FILE* f = fopen("./output", "w");
   assert(f != NULL);
   if (is_float)
-    fwrite(outbuf, sizeof(float), out_dimx * out_dimy, f);
+    fwrite(outbuf, sizeof(float), out_dimx * out_dimy * out_dimz, f);
   else
-    fwrite(outbuf, sizeof(double), out_dimx * out_dimy, f);
+    fwrite(outbuf, sizeof(double), out_dimx * out_dimy * out_dimz, f);
   fclose(f);
 
   /* Clean up */
