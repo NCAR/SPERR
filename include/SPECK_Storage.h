9707480842804ef5764980e4a5175918b110ff63
sam <sam@ThinkCenter>
2022-04-27 18:25:52 -0600

    add a function that calculates coefficient energy

--- include/SPECK_Storage.h
+++ include/SPECK_Storage.h
@@ -21,12 +21,12 @@ class SPECK_Storage {
   template <typename T>
   auto copy_data(const T*, size_t len, dims_type dims) -> RTNType;
   auto take_data(vecd_type&&, dims_type dims) -> RTNType;
-  auto release_data() -> vecd_type&&;                        // Release ownership
-  [[nodiscard]] auto view_data() const -> const vecd_type&;  // Keep ownership
-  [[nodiscard]] auto get_dims() const -> std::array<size_t, 3>;
+  auto release_data() -> vecd_type&&;          // Release ownership
+  auto view_data() const -> const vecd_type&;  // Keep ownership
+  auto get_dims() const -> std::array<size_t, 3>;
 
   // Get the encoded bitstream.
-  [[nodiscard]] auto view_encoded_bitstream() const -> const vec8_type&;
+  auto view_encoded_bitstream() const -> const vec8_type&;
   auto release_encoded_bitstream() -> vec8_type&&;
 
   // Prepare internal states for a decompression operation from an encoded bitstream
@@ -42,6 +42,12 @@ class SPECK_Storage {
   auto get_speck_stream_size(const void*) const -> uint64_t;
   auto get_speck_stream_dims(const void*) const -> std::array<size_t, 3>;
 
+  // Methods for research use only.
+  //
+  // Calculate the "energy" of data held by `m_coeff_buf`.
+  // It could be energy of the input data, or the transformed wavelet coefficients.
+  auto calc_energy() const -> double;
+
  protected:
   //
   // Member variables
@@ -54,7 +60,7 @@ class SPECK_Storage {
   const size_t m_header_size = 22;  // See header definition in SPECK_Storage.cpp.
 #endif
 
-  int32_t m_max_coeff_bits = 0;     // Necessary in preparing bitstream headers.
+  int32_t m_max_coeff_bits = 0;  // Necessary in preparing bitstream headers.
   dims_type m_dims = {0, 0, 0};
 
   std::vector<double> m_coeff_buf;
