2157ab87a2b5db8d9d6e4aa3e001912376ef7a0e
Samuel Li <Sam@Navada>
2022-11-04 21:28:01 -0600

    experimental mid-tread compiles

--- utilities/mid-tread.cpp
+++ utilities/mid-tread.cpp
@@ -19,8 +19,8 @@ int main(int argc, char* argv[])
       ->check(CLI::ExistingFile)
       ->group("Input Specifications");
 
-  auto dims = std::vector<size_t>();
-  app.add_option("--dims", dims,
+  auto dimsv = std::vector<size_t>();
+  app.add_option("--dims", dimsv,
                  "Dimensions of the input volume. E.g., `--dims 128 128 128`\n"
                  "(The fastest-varying dimension appears first.)")
       ->expected(3)
@@ -47,6 +47,7 @@ int main(int argc, char* argv[])
   CLI11_PARSE(app, argc, argv);
 
   // Read the input file
+  const auto dims = sperr::dims_type{dimsv[0], dimsv[1], dimsv[2]};
   const size_t total_vals = dims[0] * dims[1] * dims[2];
   auto orig = sperr::read_whole_file<uint8_t>(input_file);
   if ((use_double && orig.size() != total_vals * sizeof(double)) ||
@@ -55,128 +56,30 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  // Use a compressor and take the input data
+  // Use a compressor
   auto encoder = sperr::SPERR3D_INT_Driver();
   encoder.set_dims(dims);
   encoder.set_pwe(pwe);
-
   if (use_double)
     encoder.copy_data(reinterpret_cast<const double*>(orig.data()), total_vals);
-  else {
+  else
     encoder.copy_data(reinterpret_cast<const float*>(orig.data()), total_vals);
+  encoder.compress();
+  auto bitstream = encoder.release_encoded_bitstream();
 
-  // Free up memory if we don't need to compute stats
-  if (!show_stats) {
-    orig.clear();
-    orig.shrink_to_fit();
-  }
+  // Write out the encoded bitstream, and free up memory 
+  sperr::write_n_bytes("bit.stream", bitstream.size(), bitstream.data());
+  orig.clear();
+  orig.shrink_to_fit();
 
-  // Tell the compressor which compression mode to use.
-  switch (mode) {
-    case sperr::CompMode::FixedSize:
-      rtn = compressor.set_target_bpp(bpp);
-      break;
-    case sperr::CompMode::FixedPSNR:
-      compressor.set_target_psnr(psnr);
-      break;
-    default:
-      compressor.set_target_pwe(pwe);
-      break;
-  }
-  if (rtn != sperr::RTNType::Good) {
-    std::cerr << "Set bit-per-pixel failed!" << std::endl;
-    return 1;
-  }
-
-  // Perform the actual compression
-  rtn = compressor.compress();
-  switch (rtn) {
-    case sperr::RTNType::QzLevelTooBig:
-      std::cerr << "Compression failed because `qz` is set too big!" << std::endl;
-      return 1;
-    case sperr::RTNType::Good:
-      break;
-    default:
-      std::cerr << "Compression failed!" << std::endl;
-      return 1;
-  }
-
-  // Get a hold of the encoded bitstream.
-  auto stream = compressor.get_encoded_bitstream();
-  if (stream.empty()) {
-    std::cerr << "Compression bitstream empty!" << std::endl;
-    return 1;
-  }
-
-  // Write out the encoded bitstream.
-  if (!output_file.empty()) {
-    rtn = sperr::write_n_bytes(output_file, stream.size(), stream.data());
-    if (rtn != sperr::RTNType::Good) {
-      std::cerr << "Write compressed file failed!" << std::endl;
-      return 1;
-    }
-  }
-
-  // Calculate and print statistics
-  if (show_stats) {
-    const auto bpp = stream.size() * 8.0 / total_vals;
-
-    // Use a decompressor to decompress and collect error statistics
-    SPERR3D_OMP_D decompressor;
-    decompressor.set_num_threads(omp_num_threads);
-    rtn = decompressor.use_bitstream(stream.data(), stream.size());
-    if (rtn != RTNType::Good)
-      return 1;
-
-    rtn = decompressor.decompress(stream.data());
-    if (rtn != RTNType::Good)
-      return 1;
-
-    if (use_double) {
-      const auto& recover = decompressor.view_data();
-      assert(recover.size() * sizeof(double) == orig.size());
-      auto stats = sperr::calc_stats(reinterpret_cast<const double*>(orig.data()), recover.data(),
-                                     recover.size(), omp_num_threads);
-      auto var = sperr::calc_variance(reinterpret_cast<const double*>(orig.data()), total_vals,
-                                      omp_num_threads);
-      auto sigma = std::sqrt(var);
-      auto gain = std::log2(sigma / stats[0]) - bpp;
-      std::cout << "Average BPP = " << bpp << ", PSNR = " << stats[2]
-                << "dB, L-Infty = " << stats[1] << ", Accuracy Gain = " << gain << std::endl;
-      std::printf("Input data range = %.2e (%.2e, %.2e).\n", (stats[4] - stats[3]), stats[3],
-                  stats[4]);
-    }
-    else {
-      const auto recover = decompressor.get_data<float>();
-      assert(recover.size() * sizeof(float) == orig.size());
-      auto stats = sperr::calc_stats(reinterpret_cast<const float*>(orig.data()), recover.data(),
-                                     recover.size(), omp_num_threads);
-      auto var = sperr::calc_variance(reinterpret_cast<const float*>(orig.data()), total_vals,
-                                      omp_num_threads);
-      auto sigma = std::sqrt(var);
-      auto gain = std::log2(sigma / stats[0]) - float(bpp);
-      std::cout << "Average BPP = " << bpp << ", PSNR = " << stats[2]
-                << "dB, L-Infty = " << stats[1] << ", Accuracy Gain = " << gain << std::endl;
-      std::printf("Input data range = %.2e (%.2e, %.2e).\n", (stats[4] - stats[3]), stats[3],
-                  stats[4]);
-    }
-
-    if (mode == sperr::CompMode::FixedPWE) {
-      // Also collect outlier statistics
-      const auto out_stats = compressor.get_outlier_stats();
-      if (out_stats.first == 0) {
-        std::cout << "There were no outliers corrected!\n";
-      }
-      else {
-        std::printf(
-            "There were %ld outliers, percentage of total data points = %.2f%%.\n"
-            "Correcting them takes bpp = %.2f, percentage of total storage = %.2f%%.\n",
-            out_stats.first, double(out_stats.first * 100) / double(total_vals),
-            double(out_stats.second * 8) / double(out_stats.first),
-            double(out_stats.second * 100) / double(stream.size()));
-      }
-    }
-  }
+  // Use a decompressor
+  auto decoder = sperr::SPERR3D_INT_Driver();
+  decoder.set_dims(dims);
+  decoder.set_pwe(pwe);
+  decoder.use_bitstream(bitstream.data(), bitstream.size());
+  decoder.decompress();
+  auto output = decoder.release_decoded_data();
+  sperr::write_n_bytes("decoded.data", 8 * output.size(), output.data());
 
   return 0;
 }
