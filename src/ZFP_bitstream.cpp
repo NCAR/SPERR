4e1d59d5ad2b763d1415a39d19c13b9cdeaf6562
Samuel Li <shaomeng@users.noreply.github.com>
2023-02-26 21:56:19 -0700

    Wrap zfp bitstream in C++ class (#191)
    * add ZFP_bitstream class, hoping to replace std::vector<bool>
    
    * clang-format
    
    * add notes
    
    ---------
    
    Co-authored-by: Samuel Li <Sam@Navada>

---
+++ src/ZFP_bitstream.cpp
@@ -0,0 +1,66 @@
+#include "ZFP_bitstream.h"
+
+// Constructor
+sperr::ZFP_bitstream::ZFP_bitstream(size_t nbits)
+{
+  // Number of longs that's absolutely needed.
+  auto num_of_longs = nbits / 64;
+  if (nbits % 64 != 0)
+    num_of_longs++;
+
+  m_buf.resize(num_of_longs);
+  m_capacity = num_of_longs * 64;
+
+  m_handle.reset(zfp::stream_open(m_buf.data(), m_buf.size() * sizeof(uint64_t)));
+}
+
+// Functions for both read and write
+void sperr::ZFP_bitstream::rewind()
+{
+  return zfp::stream_rewind(m_handle.get());
+}
+
+auto sperr::ZFP_bitstream::capacity() const -> size_t
+{
+  return m_capacity;
+}
+
+// Functions for read
+auto sperr::ZFP_bitstream::rtell() const -> size_t
+{
+  return zfp::stream_rtell(m_handle.get());
+}
+
+void sperr::ZFP_bitstream::rseek(size_t offset)
+{
+  zfp::stream_rseek(m_handle.get(), offset);
+}
+
+// Functions for write
+auto sperr::ZFP_bitstream::wtell() const -> size_t
+{
+  return zfp::stream_wtell(m_handle.get());
+}
+
+void sperr::ZFP_bitstream::wseek(size_t offset)
+{
+  return zfp::stream_wseek(m_handle.get(), offset);
+}
+
+auto sperr::ZFP_bitstream::flush() -> size_t
+{
+  return zfp::stream_flush(m_handle.get());
+}
+
+void sperr::ZFP_bitstream::m_wgrow_buf()
+{
+  const auto curr_pos = zfp::stream_wtell(m_handle.get());
+  zfp::stream_flush(m_handle.get());
+
+  m_buf.push_back(0ul);
+  m_buf.resize(m_buf.capacity());
+  m_capacity = m_buf.size() * 64;
+  m_handle.reset(zfp::stream_open(m_buf.data(), m_buf.size() * sizeof(uint64_t)));
+
+  zfp::stream_wseek(m_handle.get(), curr_pos);
+}
