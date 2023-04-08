905c773a1826777b6124a4d1e9f79fbdca945147
Samuel Li <Sam@Navada>
2023-04-07 22:41:15 -0600

    minor

--- include/sperr_helper.h
+++ include/sperr_helper.h
@@ -43,7 +43,7 @@ enum class SigType : unsigned char { Insig, Sig, NewlySig, Dunno, Garbage };
 
 enum class SetType : unsigned char { TypeS, TypeI, Garbage };
 
-enum class UINTType : unsigned char { UINT64, UINT32, UINT16, UINT8};
+enum class UINTType { UINT64 = 0, UINT32, UINT16, UINT8 };
 
 // Return Type
 enum class RTNType {
@@ -52,7 +52,7 @@ enum class RTNType {
   BitstreamWrongLen,
   IOError,
   InvalidParam,
-  EmptyStream,    // a condition but not sure if it's an error
+  EmptyStream,  // a condition but not sure if it's an error
   BitBudgetMet,
   VersionMismatch,
   ZSTDMismatch,
