a241bce109131772f15cd1fc30649a1140b97885
Samuel Li <sam@cisl-m121a>
2023-05-26 22:50:24 -0600

    a little cleaner code

--- src/SPECK_FLT.cpp
+++ src/SPECK_FLT.cpp
@@ -29,7 +29,7 @@ auto sperr::SPECK_FLT::use_bitstream(const void* p, size_t len) -> RTNType
   m_vals_d.clear();
   m_sign_array.clear();
   m_condi_bitstream.clear();
-  std::visit([](auto& vec) { vec.clear(); }, m_vals_ui);
+  std::visit([](auto&& vec) { vec.clear(); }, m_vals_ui);
 
   const uint8_t* const ptr = static_cast<const uint8_t*>(p);
 
@@ -69,7 +69,7 @@ auto sperr::SPECK_FLT::use_bitstream(const void* p, size_t len) -> RTNType
   m_instantiate_int_vec();
   m_instantiate_decoder();
   const auto speck_len = len - pos;  // Assume that the bitstream length is what's left.
-  std::visit([speck_p, speck_len](auto& dec) { dec->use_bitstream(speck_p, speck_len); },
+  std::visit([speck_p, speck_len](auto&& dec) { dec->use_bitstream(speck_p, speck_len); },
              m_decoder);
 
   return RTNType::Good;
@@ -82,7 +82,7 @@ void sperr::SPECK_FLT::append_encoded_bitstream(vec8_type& buf) const
   std::copy(m_condi_bitstream.cbegin(), m_condi_bitstream.cend(), buf.begin() + orig_size);
 
   if (!m_conditioner.is_constant(m_condi_bitstream[0]))
-    std::visit([&buf](auto& enc) { enc->append_encoded_bitstream(buf); }, m_encoder);
+    std::visit([&buf](auto&& enc) { enc->append_encoded_bitstream(buf); }, m_encoder);
 }
 
 auto sperr::SPECK_FLT::release_decoded_data() -> vecd_type&&
@@ -106,30 +106,30 @@ auto sperr::SPECK_FLT::integer_len() const -> size_t
     case UINTType::UINT64:
       assert(m_vals_ui.index() == 0);
       assert(m_encoder.index() == 0 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_encoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_encoder));
       assert(m_decoder.index() == 0 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_decoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_decoder));
       return sizeof(uint64_t);
     case UINTType::UINT32:
       assert(m_vals_ui.index() == 1);
       assert(m_encoder.index() == 1 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_encoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_encoder));
       assert(m_decoder.index() == 1 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_decoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_decoder));
       return sizeof(uint32_t);
     case UINTType::UINT16:
       assert(m_vals_ui.index() == 2);
       assert(m_encoder.index() == 2 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_encoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_encoder));
       assert(m_decoder.index() == 2 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_decoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_decoder));
       return sizeof(uint16_t);
     default:
       assert(m_vals_ui.index() == 3);
       assert(m_encoder.index() == 3 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_encoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_encoder));
       assert(m_decoder.index() == 3 ||
-             std::visit([](auto& coder) { return coder == nullptr; }, m_decoder));
+             std::visit([](auto&& coder) { return coder == nullptr; }, m_decoder));
       return sizeof(uint8_t);
   }
 }
@@ -164,68 +164,78 @@ auto sperr::SPECK_FLT::m_midtread_f2i() -> RTNType
   assert(FE_TONEAREST == std::fegetround());
   assert(FLT_ROUNDS == 1);
 
-  const auto total_vals = m_vals_d.size();
-  const auto q1 = 1.0 / m_q;
-  auto vals_ll = std::vector<long long int>(total_vals);
-  m_sign_array.resize(total_vals);
+  // Find the biggest floating point value, then get its quantized integer.
+  auto maxd = *std::max_element(m_vals_d.cbegin(), m_vals_d.cend(),
+                                [](auto a, auto b) { return std::abs(a) < std::abs(b); });
   std::feclearexcept(FE_INVALID);
-  std::transform(m_vals_d.cbegin(), m_vals_d.cend(), vals_ll.begin(),
-                 [q1](auto d) { return std::llrint(d * q1); });
+  auto maxll = std::llrint(std::abs(maxd) / m_q);
   if (std::fetestexcept(FE_INVALID))
     return RTNType::FE_Invalid;
 
-  // Extract signs from the quantized integers.
-  std::transform(vals_ll.cbegin(), vals_ll.cend(), m_sign_array.begin(),
-                 [](auto ll) { return ll >= 0; });
-
-  // Decide integer length
-  const auto maxmag = *std::max_element(vals_ll.cbegin(), vals_ll.cend(),
-                                        [](auto a, auto b) { return std::abs(a) < std::abs(b); });
-  if (maxmag <= std::numeric_limits<uint8_t>::max())
+  // Decide integer length, and instantiate `m_vals_ui`.
+  if (maxll <= std::numeric_limits<uint8_t>::max())
     m_uint_flag = UINTType::UINT8;
-  else if (maxmag <= std::numeric_limits<uint16_t>::max())
+  else if (maxll <= std::numeric_limits<uint16_t>::max())
     m_uint_flag = UINTType::UINT16;
-  else if (maxmag <= std::numeric_limits<uint32_t>::max())
+  else if (maxll <= std::numeric_limits<uint32_t>::max())
     m_uint_flag = UINTType::UINT32;
   else
     m_uint_flag = UINTType::UINT64;
 
-  // Use the correct integer length for `m_vals_ui`, and keep the integer magnitude.
   m_instantiate_int_vec();
-  std::visit([total_vals](auto& vec) { vec.resize(total_vals); }, m_vals_ui);
+  const auto total_vals = m_vals_d.size();
+  std::visit([total_vals](auto&& vec) { vec.resize(total_vals); }, m_vals_ui);
+  m_sign_array.resize(total_vals);
+
+  for (size_t i = 0; i < total_vals; i++) {
+    auto ll = std::llrint(m_vals_d[i] / m_q);
+    m_sign_array[i] = (ll >= 0);
+    std::visit([i, ll](auto&& vec) { vec[i] = (std::abs(ll)); }, m_vals_ui);
+  }
+
+  /* Not sure if std::visit() inside of a loop makes a performance bottleneck, so
+   * keep this switch implementation for now.
   switch (m_uint_flag) {
     case UINTType::UINT64:
-      assert(m_vals_ui.index() == 0);
-      std::transform(vals_ll.cbegin(), vals_ll.cend(), std::get_if<0>(&m_vals_ui)->begin(),
-                     [](auto ll) { return static_cast<uint64_t>(std::abs(ll)); });
+      for (size_t i = 0; i < total_vals; i++) {
+        auto ll = std::llrint(m_vals_d[i] / m_q);
+        m_sign_array[i] = (ll >= 0);
+        std::get<0>(m_vals_ui)[i] = static_cast<uint64_t>(std::abs(ll));
+      }
       break;
     case UINTType::UINT32:
-      assert(m_vals_ui.index() == 1);
-      std::transform(vals_ll.cbegin(), vals_ll.cend(), std::get_if<1>(&m_vals_ui)->begin(),
-                     [](auto ll) { return static_cast<uint32_t>(std::abs(ll)); });
+      for (size_t i = 0; i < total_vals; i++) {
+        auto ll = std::llrint(m_vals_d[i] / m_q);
+        m_sign_array[i] = (ll >= 0);
+        std::get<1>(m_vals_ui)[i] = static_cast<uint32_t>(std::abs(ll));
+      }
       break;
     case UINTType::UINT16:
-      assert(m_vals_ui.index() == 2);
-      std::transform(vals_ll.cbegin(), vals_ll.cend(), std::get_if<2>(&m_vals_ui)->begin(),
-                     [](auto ll) { return static_cast<uint16_t>(std::abs(ll)); });
+      for (size_t i = 0; i < total_vals; i++) {
+        auto ll = std::llrint(m_vals_d[i] / m_q);
+        m_sign_array[i] = (ll >= 0);
+        std::get<2>(m_vals_ui)[i] = static_cast<uint16_t>(std::abs(ll));
+      }
       break;
     default:
-      assert(m_vals_ui.index() == 3);
-      std::transform(vals_ll.cbegin(), vals_ll.cend(), std::get_if<3>(&m_vals_ui)->begin(),
-                     [](auto ll) { return static_cast<uint8_t>(std::abs(ll)); });
-  }
+      for (size_t i = 0; i < total_vals; i++) {
+        auto ll = std::llrint(m_vals_d[i] / m_q);
+        m_sign_array[i] = (ll >= 0);
+        std::get<3>(m_vals_ui)[i] = static_cast<uint8_t>(std::abs(ll));
+      }
+  } */
 
   return RTNType::Good;
 }
 
 void sperr::SPECK_FLT::m_midtread_i2f()
 {
-  assert(m_sign_array.size() == std::visit([](auto& vec) { return vec.size(); }, m_vals_ui));
+  assert(m_sign_array.size() == std::visit([](auto&& vec) { return vec.size(); }, m_vals_ui));
 
   const auto tmpd = std::array<double, 2>{-1.0, 1.0};
   m_vals_d.resize(m_sign_array.size());
   std::visit(
-      [&vals_d = m_vals_d, &sign_array = m_sign_array, tmpd, q = m_q](auto& vec) {
+      [&vals_d = m_vals_d, &sign_array = m_sign_array, tmpd, q = m_q](auto&& vec) {
         std::transform(
             vec.cbegin(), vec.cend(), sign_array.cbegin(), vals_d.begin(),
             [tmpd, q](auto i, auto b) { return (q * static_cast<double>(i) * tmpd[b]); });
@@ -267,7 +277,7 @@ auto sperr::SPECK_FLT::compress() -> RTNType
 
   // Step 4: Integer SPECK encoding
   m_instantiate_encoder();
-  std::visit([&dims = m_dims](auto& encoder) { encoder->set_dims(dims); }, m_encoder);
+  std::visit([&dims = m_dims](auto&& encoder) { encoder->set_dims(dims); }, m_encoder);
   switch (m_uint_flag) {
     case UINTType::UINT64:
       assert(m_vals_ui.index() == 0);
@@ -293,7 +303,7 @@ auto sperr::SPECK_FLT::compress() -> RTNType
       std::get<3>(m_encoder)->use_coeffs(std::move(std::get<3>(m_vals_ui)),
                                          std::move(m_sign_array));
   }
-  std::visit([](auto& encoder) { encoder->encode(); }, m_encoder);
+  std::visit([](auto&& encoder) { encoder->encode(); }, m_encoder);
 
   return RTNType::Good;
 }
@@ -301,7 +311,7 @@ auto sperr::SPECK_FLT::compress() -> RTNType
 auto sperr::SPECK_FLT::decompress() -> RTNType
 {
   m_vals_d.clear();
-  std::visit([](auto& vec) { vec.clear(); }, m_vals_ui);
+  std::visit([](auto&& vec) { vec.clear(); }, m_vals_ui);
   m_sign_array.clear();
 
   // `m_condi_bitstream` might be indicating a constant field, so let's see if that's
@@ -313,27 +323,31 @@ auto sperr::SPECK_FLT::decompress() -> RTNType
 
   // Step 1: Integer SPECK decode.
   //    Note: the decoder has already parsed the bitstream in function `use_bitstream()`.
-  std::visit([&dims = m_dims](auto& decoder) { decoder->set_dims(dims); }, m_decoder);
-  std::visit([](auto& decoder) { decoder->decode(); }, m_decoder);
+  std::visit([&dims = m_dims](auto&& decoder) { decoder->set_dims(dims); }, m_decoder);
+  std::visit([](auto&& decoder) { decoder->decode(); }, m_decoder);
   switch (m_uint_flag) {  // `m_uint_flag` was properly set during `use_bitstream()`.
     case UINTType::UINT64:
       assert(m_decoder.index() == 0);
       m_vals_ui = std::get<0>(m_decoder)->release_coeffs();
+      assert(m_vals_ui.index() == 0);
       break;
     case UINTType::UINT32:
       assert(m_decoder.index() == 1);
       m_vals_ui = std::get<1>(m_decoder)->release_coeffs();
+      assert(m_vals_ui.index() == 1);
       break;
     case UINTType::UINT16:
       assert(m_decoder.index() == 2);
       m_vals_ui = std::get<2>(m_decoder)->release_coeffs();
+      assert(m_vals_ui.index() == 2);
       break;
     default:
       assert(m_decoder.index() == 3);
       m_vals_ui = std::get<3>(m_decoder)->release_coeffs();
+      assert(m_vals_ui.index() == 3);
       break;
   }
-  m_sign_array = std::visit([](auto& decoder) { return decoder->release_signs(); }, m_decoder);
+  m_sign_array = std::visit([](auto&& dec) { return dec->release_signs(); }, m_decoder);
 
   // Step 2: Inverse quantization
   auto rtn = m_inverse_quantize();
