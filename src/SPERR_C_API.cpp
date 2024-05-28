3d54dc5e29154ca09616db137157950ea60e9858
Samuel Li <shaomenglee@gmail.com>
2024-05-27 21:52:11 -0600

    clang-format

--- src/SPERR_C_API.cpp
+++ src/SPERR_C_API.cpp
@@ -9,14 +9,14 @@
 #include "SPERR3D_Stream_Tools.h"
 
 auto C_API::sperr_comp_2d(const void* src,
-                         int is_float,
-                         size_t dimx,
-                         size_t dimy,
-                         int mode,
-                         double quality,
-                         int out_inc_header,
-                         void** dst,
-                         size_t* dst_len) -> int
+                          int is_float,
+                          size_t dimx,
+                          size_t dimy,
+                          int mode,
+                          double quality,
+                          int out_inc_header,
+                          void** dst,
+                          size_t* dst_len) -> int
 {
   // Examine if `dst` is pointing to a NULL pointer
   if (*dst != nullptr)
@@ -96,11 +96,11 @@ auto C_API::sperr_comp_2d(const void* src,
 }
 
 auto C_API::sperr_decomp_2d(const void* src,
-                           size_t src_len,
-                           int output_float,
-                           size_t dimx,
-                           size_t dimy,
-                           void** dst) -> int
+                            size_t src_len,
+                            int output_float,
+                            size_t dimx,
+                            size_t dimy,
+                            void** dst) -> int
 {
   // Examine if `dst` is pointing to a NULL pointer
   if (*dst != nullptr)
@@ -154,18 +154,18 @@ void C_API::sperr_parse_header(const void* src,
 }
 
 auto C_API::sperr_comp_3d(const void* src,
-                         int is_float,
-                         size_t dimx,
-                         size_t dimy,
-                         size_t dimz,
-                         size_t chunk_x,
-                         size_t chunk_y,
-                         size_t chunk_z,
-                         int mode,
-                         double quality,
-                         size_t nthreads,
-                         void** dst,
-                         size_t* dst_len) -> int
+                          int is_float,
+                          size_t dimx,
+                          size_t dimy,
+                          size_t dimz,
+                          size_t chunk_x,
+                          size_t chunk_y,
+                          size_t chunk_z,
+                          int mode,
+                          double quality,
+                          size_t nthreads,
+                          void** dst,
+                          size_t* dst_len) -> int
 {
   // Examine if `dst` is pointing to a NULL pointer
   if (*dst != nullptr)
@@ -216,13 +216,13 @@ auto C_API::sperr_comp_3d(const void* src,
 }
 
 auto C_API::sperr_decomp_3d(const void* src,
-                           size_t src_len,
-                           int output_float,
-                           size_t nthreads,
-                           size_t* dimx,
-                           size_t* dimy,
-                           size_t* dimz,
-                           void** dst) -> int
+                            size_t src_len,
+                            int output_float,
+                            size_t nthreads,
+                            size_t* dimx,
+                            size_t* dimy,
+                            size_t* dimz,
+                            void** dst) -> int
 {
   // Examine if `dst` is pointing to a NULL pointer.
   if (*dst != nullptr)
@@ -258,10 +258,10 @@ auto C_API::sperr_decomp_3d(const void* src,
 }
 
 auto C_API::sperr_trunc_3d(const void* src,
-                          size_t src_len,
-                          unsigned pct,
-                          void** dst,
-                          size_t* dst_len) -> int
+                           size_t src_len,
+                           unsigned pct,
+                           void** dst,
+                           size_t* dst_len) -> int
 {
   if (*dst != nullptr)
     return 1;
